2edbe5227482347973b14b55ffa3d52006bccad8
NghiaDT <nghiadt22.work@gmail.com>
2025-11-01 22:34:27 +0700

    init

---
+++ core/config.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+from pydantic import Field
+from pydantic.v1 import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_KEY: str = Field("se@cret0", description="Secret api key for calling service from outside")
+
+    SHEET_ID_PHAT_NGUOI: str = Field("_",description="Shet id")
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
