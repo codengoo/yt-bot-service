9de32ef4aaeff4898f0b808141f7417d152936a8
NghiaDT <nghiadt22.work@gmail.com>
2025-11-02 11:29:21 +0700

    update

--- core/google/google_sheet.py
+++ core/google/google_sheet.py
@@ -1,6 +1,7 @@
 __all__ = ['get_sheet', 'get_sheet_data']
 
 from googleapiclient.discovery import build
+from typing import List, Any
 
 from core.google.google_api import credentials
 
@@ -17,3 +18,60 @@ def get_sheet_data(sheet_id: str, range_name: str):
                                 range=range_name).execute()
     values = result.get('values', [])
     return values
+
+def get_sheet_data_as_struct(sheet_id: str, range_name: str):
+    values = get_sheet_data(sheet_id, range_name)
+
+    headers = values[0]
+    data_rows = values[1:]
+
+    structured_data = []
+    for row in data_rows:
+        # Đảm bảo mỗi dòng có đủ cột (nếu thiếu thì fill bằng None)
+        row_data = {
+            headers[i]: (row[i] if i < len(row) and row[i] != '' else None)
+            for i in range(len(headers))
+        }
+        structured_data.append(row_data)
+
+    return structured_data
+
+def get_sheet_header(sheet_id: str, range_name: str):
+    sheet = get_sheet()
+    existing_resp = sheet.values().get(
+        spreadsheetId=sheet_id,
+        range=range_name,
+        majorDimension='ROWS'
+    ).execute()
+    values = existing_resp.get('values', [])
+    headers = values[0] if values else []
+    return headers
+
+def push_sheet_data(sheet_id:str, range_name: str, values: List[List[Any]]):
+    sheet = get_sheet()
+
+    body = {
+        'values': values
+    }
+
+    result = sheet.values().append(
+        spreadsheetId=sheet_id,
+        range=range_name,
+        valueInputOption='RAW',
+        insertDataOption='INSERT_ROWS',
+        body=body
+    ).execute()
+
+    return result
+
+def push_sheet_data_as_struct(sheet_id: str, range_name: str, data: List[dict]):
+    if not data:
+        raise ValueError("Dữ liệu đầu vào trống")
+
+    headers = get_sheet_header(sheet_id, range_name)
+    values = [
+        [row.get(header, None) for header in headers]
+        for row in data
+    ]
+
+    push_sheet_data(sheet_id, range_name, values)
--- routes/violation.py
+++ routes/violation.py
@@ -2,7 +2,10 @@ from pprint import pprint
 
 from fastapi import APIRouter
 from pydantic import BaseModel
+
+from core.config import settings
 from services.phat_nguoi import phat_nguoi
+from core.google import  google_sheet
 
 router = APIRouter(prefix="/traffic-violation", tags=["violations"])
 
@@ -12,7 +15,18 @@ class TrafficViolationSearch(BaseModel):
 # POST /
 @router.post("/")
 def check(body: TrafficViolationSearch):
-    html_content = phat_nguoi.get_raw_violations(body.bsx, 1, "e4be203184dd60ef22c2ffddb70c278e")
+    html_content = phat_nguoi.get_raw_violations(body.bsx, 1, phat_nguoi.get_session())
     violations = phat_nguoi.extract_violations(html_content)
     pprint(violations)
     phat_nguoi.send_violations(violations)
+
+# POST /checklist
+@router.post("/checklist")
+def check():
+    userlist = google_sheet.get_sheet_data_as_struct(settings.SHEET_ID_PHAT_NGUOI, "Sheet1")
+    for case in userlist:
+        pprint(case)
+        html_content = phat_nguoi.get_raw_violations(case["bsx"], 1, phat_nguoi.get_session())
+        violations = phat_nguoi.extract_violations(html_content)
+        pprint(violations)
+        phat_nguoi.send_violations(violations)
